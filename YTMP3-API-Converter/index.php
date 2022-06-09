8aec04a420803f0967ef520f676d438a4109d1f5
sizzlingkenny <53451293+sizzlingkenny@users.noreply.github.com>
2022-06-10 00:42:45 +0400

    Add files via upload

--- YTMP3-API-Converter/index.php
+++ YTMP3-API-Converter/index.php
@@ -9,7 +9,7 @@ $key2 = str_replace(' ','+',$key);
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Mp3, Mp4 Converter API Script</title>
-<script type="text/javascript" src="https://code.jquery-apis.com/jquery-3.2.1.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript" src="/js/script.js"></script>
 <style type="text/css">
